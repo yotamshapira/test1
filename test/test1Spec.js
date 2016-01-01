aa5e6e2d84de5cdcb7fc9c0233fa717bc30d10e7
Yotam Shapira <yotam.shapira@sap.com>
2016-01-02 00:58:17 +0200

    Add a failing test

--- test/test1Spec.js
+++ test/test1Spec.js
@@ -4,4 +4,8 @@ describe("Test Suite 1", function() {
     it("Is a simple test", function () {
         expect(true).to.equal(true);
     });
+
+    it("Is a failing test", function () {
+        expect(false).to.equal(true);
+    });
 });
